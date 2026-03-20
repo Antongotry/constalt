b54992924eab95dbcfd851160ebe442d71002594
mac <mac@MacBook-Pro-mac.local>
2026-03-20 10:18:12 +0200

    Move expert section to end of front page before footer

--- front-page.php
+++ front-page.php
@@ -10,12 +10,12 @@ get_header();
 <?php get_template_part('template-parts/layout/site', 'header'); ?>
 <main id="primary" class="site-main site-main--front-page">
     <?php get_template_part('template-parts/sections/front-page', 'hero'); ?>
-    <?php get_template_part('template-parts/sections/front-page', 'expert'); ?>
     <?php get_template_part('template-parts/sections/front-page', 'insight'); ?>
     <?php get_template_part('template-parts/sections/front-page', 'services'); ?>
     <?php get_template_part('template-parts/sections/front-page', 'collaboration'); ?>
     <?php get_template_part('template-parts/sections/front-page', 'trust'); ?>
     <?php get_template_part('template-parts/sections/front-page', 'about'); ?>
+    <?php get_template_part('template-parts/sections/front-page', 'expert'); ?>
 </main>
 <?php
 get_footer();
